dca15fd1ab08800207358bbe7c2a42574aa28b47
Duane Felton <dfelton2010@gmail.com>
2018-09-18 15:38:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,8 +17,9 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
-    
-    assign_rooms(attendees).each do |room|
-      puts room
-    end
+  end
+  
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
