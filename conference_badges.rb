4df579fa273dca7dd837dcc310eee13e25820346
Duane Felton <dfelton2010@gmail.com>
2018-09-18 14:35:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,15 +10,6 @@ def batch_badge_creator(attendees)
   badges
 end
 
-#[5,6,7].each_with_index do |x,i|
-
-#puts "#{i} -> #{x}"
-#end
-
-#Outputs: 0 -> 5 1 -> 6 2 -> 7
-
-#a = [11,22,31,224,44].each.with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
-
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index {|val,index| room_assignments << "Hello, #{val}! You'll be assigned to room #{index + 1}!"}
